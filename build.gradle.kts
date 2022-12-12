777e508e9fa81a621be34b1e2508fba19ae1be61
Stamatis Zampetakis <zabetak@gmail.com>
2022-12-12 13:36:32 +0100

    Try passing agent command via env variable

--- build.gradle.kts
+++ build.gradle.kts
@@ -90,7 +90,7 @@ val skipAutostyle by props()
 val skipJavadoc by props()
 val enableMavenLocal by props()
 val enableGradleMetadata by props()
-val profilerAgent = props.string("profilerAgent", "")
+val profilerAgent = System.getenv("ASYNC_AGENT")
 val werror by props(true) // treat javac warnings as errors
 // Inherited from stage-vote-release-plugin: skipSign, useGpgCmd
 // Inherited from gradle-extensions-plugin: slowSuiteLogThreshold=0L, slowTestLogThreshold=2000L
@@ -735,7 +735,7 @@ allprojects {
                 }
                 exclude("**/*Suite*")
                 jvmArgs("-Xmx1536m")
-                if (profilerAgent.isNotEmpty()) {
+                if (profilerAgent != null) {
                     jvmArgs(profilerAgent)
                 }
                 jvmArgs("-Djdk.net.URLClassPath.disableClassPathURLCheck=true")
